3df73f01cd246cf4baca14c80ad5d62269d8aec8
Jolanta Piaseczna <piaseczna@onet.pl>
2020-08-20 23:25:52 +0200

    Add playerMove

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Computer move
+
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
@@ -9,3 +11,20 @@ if (randomNumber == '1') {
   computerMove = 'nożyce';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+// Player move
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';		
+} else {
+  playerMove = 'kamień';	
+} 
+
+printMessage('Twój ruch: ' + playerMove);
